30088e4a1474bd158207d056a47a0e771de24a1d
Jack Lv <2579821927@qq.com>
2024-07-17 22:33:37 +0800

    Merge remote-tracking branch 'origin/main'

--- Test/Test1/Main.java
+++ Test/Test1/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-
+            public ListNode reverseList(ListNode head) {
+                if(head.next == null || head == null){
+                    return head;
+                }
+                if(head.next.next == null){
+                    head.next.next = head;
+                    head.next = null;
+                }
+                ListNode pre, temp;
+                temp = head.next.next;
+                pre = head.next;
+                head.next = null;
+                pre.next = head;
+                while(temp != null){
+                    head = pre;
+                    pre = temp;
+                    temp = pre.next;
+                    pre.next = head;
+                }
+                return pre;
+            }
     }
 }
